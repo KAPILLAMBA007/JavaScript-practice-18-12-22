95def11ee8044f4315f954a204ecbe791503ac78
Kapil Lamba <115392410+KAPILLAMBA007@users.noreply.github.com>
2022-12-18 11:48:05 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,42 @@
+const a = document.getElementById("p1Element")
+p1Element.innerHTML= "Hey"
+console.log(a)
+const b =document.getElementById("p2Element")
+p2Element.innerHTML="my"
+console.log(b)
+const c =document.getElementById("c")
+c.innerHTML="name"
+console.log(c)
+const d = document.getElementById("d")
+d.innerHTML="is"
+console.log(d)
+const e=document.getElementById("e")
+var u=prompt("enter your name")
+e.innerHTML=u
+console.log(e)
+const f = document.getElementById("f")
+f.innerHTML="and"
+console.log(f)
+const g = document.getElementById("g")
+g.innerHTML="my"
+console.log(g)
+const h = document.getElementById("h")
+h.innerHTML="age"
+console.log(h)
+const i = document.getElementById("i")
+i.innerHTML="is"
+console.log(i)
+const j=document.getElementById("j")
+var z=prompt("enter your age here")
+j.innerHTML=z
+console.log(j)
+const k = document.getElementById("k")
+k.innerHTML="I"
+console.log(k)
+const l = document.getElementById("l")
+l.innerHTML="love"
+console.log(l)
+const m=document.getElementById("m")
+var y=prompt("Ente your favourite subject here")
+m.innerHTML=y
+console.log(m)
